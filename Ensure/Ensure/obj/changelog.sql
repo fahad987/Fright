008663ecf159e9575ca5134c5d5f89ac0f3ac468
Fahad Rehman <fahad.rehman365@yahoo.com>
2024-10-07 17:48:45 +0500

    Add files via upload

---
+++ Ensure/Ensure/obj/changelog.sql
@@ -0,0 +1,6 @@
+
+--GetAllRoleAsync change method to POST
+--------------------------------
+--UpdateCountryIsActiveStatus in country controller
+--UpdateCityIsActiveStatus in city controller
+--GetAllServiceType change model and method name to POST
