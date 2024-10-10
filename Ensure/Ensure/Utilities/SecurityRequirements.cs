f709c01a5b4ee77158f9c8b3398d1670ba63782a
Fahad Rehman <fahad.rehman365@yahoo.com>
2024-10-10 12:44:20 +0500

    Add files via upload

---
+++ Ensure/Ensure/Utilities/SecurityRequirements.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Ensure.Utilities;
+
+public class SecurityRequirements : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var authAtributes = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+            .Union(context.MethodInfo.GetCustomAttributes(true))
+            .OfType<AuthorizeAttribute>();
+
+        if (!authAtributes.Any()) return;
+
+        var securityRequirement = new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                },
+                new List<string>()
+            }
+        };
+        operation.Security = new List<OpenApiSecurityRequirement> {securityRequirement};
+        operation.Responses.Add("401", new OpenApiResponse {Description = "Unauthorized"});
+    }
+}
